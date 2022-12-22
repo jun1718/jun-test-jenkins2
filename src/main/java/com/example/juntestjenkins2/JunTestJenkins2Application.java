193147d7e30c566fc1d8bccc7d237761242166f1
jun1718 <gbeovhsqhtka@naver.com>
2022-12-22 15:30:39 +0900

    Initial commit

---
+++ src/main/java/com/example/juntestjenkins2/JunTestJenkins2Application.java
@@ -0,0 +1,13 @@
+package com.example.juntestjenkins2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunTestJenkins2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JunTestJenkins2Application.class, args);
+    }
+
+}
